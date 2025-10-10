e92c5bad9cbcd3c3da6aa15ebb3dfecf13bedd47
Артём Струнков <strunkovap@student.bmstu.ru>
2025-10-10 17:43:21 +0300

    added requirements and base_accessor file

---
+++ app/backend/base_accessor.py
@@ -0,0 +1,10 @@
+class BaseAccessor:
+    def __init__(self):
+        #TODO В будущем здесь будет логгер
+        pass
+
+    async def connect(self, app):
+        pass
+
+    async def disconnect(self, app):
+        pass
